488b2f47e2ed26079a86ecae15eb1cadfae4e0e1
Elvio <me@etoccalino.com>
2021-07-12 20:38:27 -0300

    validating input

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,6 +1,36 @@
 use clap::{App, Arg};
-use cli_90::*;
-use regex::Regex;
+use cli_90;
+
+fn main() {
+    let matches = App::new("90ci")
+        .about("Returns the 90% confidence interval for a model")
+        .arg(
+            Arg::with_name("equation")
+                .short("e")
+                .long("equation")
+                .takes_value(true)
+                .required(true),
+        )
+        .arg(
+            Arg::with_name("vars")
+                .long("var")
+                .takes_value(true)
+                .multiple(true)
+                .number_of_values(1),
+        )
+        .get_matches();
+
+    let equation: &str = matches.value_of("equation").unwrap();
+    let vars: Vec<&str> = matches.values_of("vars").unwrap().collect();
+    println!("Vars: {:#?}", vars);
+    println!("-----------------");
+    let res = parse_variables_descriptions(&vars).unwrap();
+    let mut names: Vec<&str> = res.iter().map(|&(name, _, _, _)| name).collect();
+    println!("Parsed: {:#?}", res);
+    assert!(validate_variables(equation, &mut names));
+}
+
+///////////////////////////////////////////////////////////////////////////////
 
 /// Ensure the variable descriptions are valid, i.e.:
 ///     var_name,distro,lower,upper
@@ -9,21 +39,9 @@ use regex::Regex;
 /// * `distro` is either "uniform" or "normal"
 /// * each of `lower` and `upper` parse to a f64
 /// * `lower < upper`
-fn parse_variables_descriptions(
-    descriptions: Vec<&str>,
-) -> Result<Vec<(&str, &str, f64, f64)>, String> {
-    //     // The regex to match floats in different variations:
-    //     // "[+-]?([0-9]+([.][0-9]*)?|[.][0-9]+)"
-    //     // See: https://stackoverflow.com/questions/12643009/regular-expression-for-floating-point-numbers
-    //     let RE: Regex = Regex::new(
-    //         r"
-    // (?P<name>[[:alnum:]]+),
-    // (?P<distribution>[[:alnum:]]+),
-    // (?P<lower>[+-]?([0-9]+([.][0-9]*)?|[.][0-9]+)),
-    // (?P<upper>[+-]?([0-9]+([.][0-9]*)?|[.][0-9]+))",
-    //     )
-    //     .unwrap();
-
+fn parse_variables_descriptions<'a>(
+    descriptions: &Vec<&'a str>,
+) -> Result<Vec<(&'a str, &'a str, f64, f64)>, String> {
     let mut res: Vec<(&str, &str, f64, f64)> = Vec::with_capacity(descriptions.len());
     let mut _lower: f64 = 0.;
     let mut _upper: f64 = 0.;
@@ -43,27 +61,27 @@ fn parse_variables_descriptions(
     Ok(res)
 }
 
-fn main() {
-    let matches = App::new("90ci")
-        .about("Returns the 90% confidence interval for a model")
-        .arg(
-            Arg::with_name("equation")
-                .short("e")
-                .long("equation")
-                .takes_value(true),
-        )
-        .arg(
-            Arg::with_name("vars")
-                .long("var")
-                .takes_value(true)
-                .multiple(true)
-                .number_of_values(1),
-        )
-        .get_matches();
-
-    let vars: Vec<&str> = matches.values_of("vars").unwrap().collect();
-    println!("Vars: {:#?}", vars);
-    println!("-----------------");
-    let res = parse_variables_descriptions(vars);
-    println!("Parsed: {:#?}", res);
+fn validate_variables(equation: &str, variables: &mut Vec<&str>) -> bool {
+    let extracted_names: Vec<&str> = cli_90::extract_variable_names(equation);
+    if extracted_names.len() != variables.len() {
+        return false;
+    }
+    let mut found: bool;
+    for name in extracted_names.iter() {
+        found = false;
+        println!("name={}", name);
+        for var in variables.iter() {
+            println!("    var={}", var);
+            if name == var {
+                println!("    break!");
+                found = true;
+                break;
+            }
+        }
+        println!("found={}", found);
+        if !found {
+            return false;
+        }
+    }
+    true
 }
